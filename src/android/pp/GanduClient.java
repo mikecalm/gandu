113ad005324303d080a1e980e3e4febd62a46ca4
urbaniakm.we <urbaniakm.we@localhost>
2010-05-27 19:24:50 +0000

    commit Spaja | dodano wy?uskiwanie ziarna :D

--- src/android/pp/GanduClient.java
+++ src/android/pp/GanduClient.java
@@ -2,10 +2,9 @@ package android.pp;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.net.InetAddress;
 import java.net.Socket;
 
 import javax.crypto.Mac;
@@ -27,7 +26,7 @@ import android.widget.EditText;
 
 public class GanduClient extends Activity {
 
-	public final static int SERVERPORT = 8074;
+    public final static int SERVERPORT = 8074;
     private EditText serverIp;
     String[] ip = null;
     private Button connectPhones;
@@ -68,7 +67,7 @@ public class GanduClient extends Activity {
         public void run() 
         {
             try {
-                InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
+                //InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
                 HttpClient httpclient = new DefaultHttpClient();
                 HttpGet httpget = new HttpGet("http://appmsg.gadu-gadu.pl/appsvc/appmsg_ver8.asp?fmnumber=6063724&lastmsg=0&version=10.1.1.11119");
                 HttpResponse response = httpclient.execute(httpget);
@@ -77,20 +76,21 @@ public class GanduClient extends Activity {
                 if (entity != null) 
                 {
                     InputStream instream = entity.getContent();
-                    int l;
+                    //int l;
                     byte[] tmp = new byte[2048];
-                    while ((l = instream.read(tmp)) != -1) 
+                    //while ((l = instream.read(tmp)) != -1) 
+                    while (instream.read(tmp) != -1)
                     {
-                    	ByteArrayInputStream bais = new ByteArrayInputStream(tmp);
-                    	BufferedReader br = new BufferedReader(new InputStreamReader(bais));
-                    	String readline = br.readLine();
-                    	Log.i("GANDU-TESTING",readline);
-                    	ip = readline.split(" ");                    	
+                        ByteArrayInputStream bais = new ByteArrayInputStream(tmp);
+                        BufferedReader br = new BufferedReader(new InputStreamReader(bais));
+                        String readline = br.readLine();
+                        Log.i("GANDU-TESTING",readline);
+                        ip = readline.split(" ");                        
                     }
                 }                
               Mac mac = Mac.getInstance("HmacSHA1");
               
-              String key = "123"; //Przykładowe dane
+              String key = "123"; //PrzykĹ‚adowe dane
               String data = "456";
               
               SecretKeySpec spec = new SecretKeySpec(key.getBytes(), "HmacSHA1");
@@ -100,28 +100,52 @@ public class GanduClient extends Activity {
               
               String oauth = Base64.encodeBytes(byteHMAC);
               Log.i("GANDU-TESTING",oauth);
-              Log.d("ClientActivity", "Połączenie....");
+              Log.d("ClientActivity", "Polaczenie....");
               
-              Socket socket = new Socket(ip[2].split(":")[0], Integer.parseInt(ip[2].split(":")[1]));
+              String ipWyizolowany = ip[2].split(":")[0];
+              int portWyizolowany = Integer.parseInt(ip[2].split(":")[1]);
+              Socket socket = new Socket(ipWyizolowany, portWyizolowany);
+              //Socket socket = new Socket(ipWyizolowany, 443);
+
               connected = true;
                 while (connected) 
                 {
                     try 
                     {
-                        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                        Log.i("GANDU-TESTING",ois.readLine());
+                        //BufferedReader in = new BufferedReader(new InputStreamReader(sslsocket.getInputStream()));
+                        //BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        DataInputStream in = new DataInputStream(socket.getInputStream());
+                        int typKom = Integer.reverseBytes(in.readInt());
+                        int dlugoscDanych = Integer.reverseBytes(in.readInt());
+                        int ziarno = Integer.reverseBytes(in.readInt());
+                        Log.i("GANDU-TESTING typ komunikatu: ",""+typKom);
+                        Log.i("GANDU-TESTING dlugosc danych: ",""+dlugoscDanych);
+                        Log.i("GANDU-TESTING ziarno: ",""+ziarno);
+                        //po podsluchaniu wiresharkiem i wychwyceniu paczki z ziarnem
+                        //i przeliczeniu go z systemu szesnastkowego na dziesietny
+                        //wartosc ziarna zgadza sie z ta uzyskana w zmiennej int ziarno
+                        //wiec jest dobrze, teraz mozna dalej kombinowac ze skompletowaniem
+                        //paczki ktora pozniej trzeba odeslac;)
+                        
+                        //to jakas proba wysylanie czegokolwiek do socketa
+                        /*DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                        out.writeByte(8);
+                        out.flush();
+                        out.writeByte(9);
+                        out.flush();*/
                     } catch (Exception e)
                     {
                         Log.e("ClientActivity", "S: Error", e);
+                        connected = false;
                     }
                 }
+                //sslsocket.close();
                 socket.close();
                 Log.d("ClientActivity", "C: Closed.");
             } 
-            //ZMIANA :)
             catch (Exception e) 
             {
-               
+               ;
             }
         }
     }
