9ea99ec83a99a943ed44fbf12d194511d9d182e9
spaj88 <spaj88@localhost>
2010-07-07 22:04:50 +0000

    + przeniesienie komunikacji z serwerem GG do serwisu

--- src/android/pp/GanduClient.java
+++ src/android/pp/GanduClient.java
@@ -17,7 +17,16 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,6 +37,10 @@ import android.widget.Toast;
 
 public class GanduClient extends Activity {
 
+	/** Messenger for communicating with service. */
+    Messenger mService = null;
+    /** Flag indicating whether we have called bind on the service. */
+    boolean mIsBound;
 	public Toast toastGandu;
 	String[] ip = null;
 	private Button connectPhones;
@@ -50,18 +63,146 @@ public class GanduClient extends Activity {
 		/*toastGandu = new Toast(this);
 		toastGandu.setView(this.getCurrentFocus());
 		toastGandu.makeText(this, "toastTest", 3000);*/
+		
+		//uruchomienie serwisu Gandu
+		startService(new Intent("android.pp.GanduS"));
+		//zbindowanie aktywnosci do serwisu
+		doBindService();
 	}
 
 	private OnClickListener connectListener = new OnClickListener() {
 		public void onClick(View v) {
-			if (!connected && serverIpAddress != "") {
-				Thread cThread = new Thread(new ClientThread());
-				cThread.start();
-
+			Message msg = Message.obtain(null,GanduService.MSG_LOGIN, 0, 0);
+			Bundle wysylany = new Bundle();
+			wysylany.putString("numerGG", ggNumberEdit.getText().toString());
+			wysylany.putString("hasloGG" , ggPasswordEdit.getText().toString());
+			msg.setData(wysylany);
+			try
+			{
+				mService.send(msg);
+			}catch(Exception excccc)
+			{
+				Log.e("Blad","Blad!!!!\n"+excccc.getMessage());
 			}
+			//if (!connected && serverIpAddress != "") {
+				//Thread cThread = new Thread(new ClientThread());
+				//cThread.start();
+            	//startService(new  Intent(GanduClient.this,serwisGandu.class));
+			//}
 		}
 	};
+	
+	//Funkcje potrzebne do zestawienia polaczenia aktywnosci z serwisem Gandu
+	/**
+     * Handler of incoming messages from service.
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case GanduService.MSG_SET_VALUE:
+                    //mCallbackText.setText("Received from service: " + msg.arg1);
+                    break;
+                case GanduService.MSG_LOGIN:
+                	break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
 
+    /**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    /**
+     * Class for interacting with the main interface of the service.
+     */
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className,
+                IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service.  We are communicating with our
+            // service through an IDL interface, so get a client-side
+            // representation of that from the raw service object.
+            mService = new Messenger(service);
+            //mCallbackText.setText("Attached.");
+
+            // We want to monitor the service for as long as we are
+            // connected to it.
+            try {
+                Message msg = Message.obtain(null,
+                        GanduService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+
+                // Give it some value as an example.
+                msg = Message.obtain(null,
+                        GanduService.MSG_SET_VALUE, this.hashCode(), 0);
+                mService.send(msg);
+            } catch (RemoteException e) {
+                // In this case the service has crashed before we could even
+                // do anything with it; we can count on soon being
+                // disconnected (and then reconnected if it can be restarted)
+                // so there is no need to do anything here.
+            }
+
+            // As part of the sample, tell the user what happened.
+            //Toast.makeText(Binding.this, R.string.remote_service_connected,
+            Toast.makeText(GanduClient.this, "remote_service_connected",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
+            //mCallbackText.setText("Disconnected.");
+
+            // As part of the sample, tell the user what happened.
+            //Toast.makeText(Binding.this, R.string.remote_service_disconnected,
+            Toast.makeText(GanduClient.this, "remote_service_disconnected",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    void doBindService() {
+        // Establish a connection with the service.  We use an explicit
+        // class name because there is no reason to be able to let other
+        // applications replace our component.
+        bindService(new Intent(GanduClient.this, 
+                GanduService.class), mConnection, Context.BIND_AUTO_CREATE);
+        
+        mIsBound = true;
+        //mCallbackText.setText("Binding.");
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            // If we have received the service, and hence registered with
+            // it, then now is the time to unregister.
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null,
+                            GanduService.MSG_UNREGISTER_CLIENT);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                } catch (RemoteException e) {
+                    // There is nothing special we need to do if the service
+                    // has crashed.
+                }
+            }
+
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+            //mCallbackText.setText("Unbinding.");
+        }
+    }
+
+    //!!!!!!!!DO WYWALENIA!!!!!!!!!!!
 	public class ClientThread implements Runnable {
 		public void run() {
 			try {
@@ -86,7 +227,8 @@ public class GanduClient extends Activity {
 
 				String ipWyizolowany = ip[2].split(":")[0];
 				int portWyizolowany = Integer.parseInt(ip[2].split(":")[1]);
-				Socket socket = new Socket(ipWyizolowany, portWyizolowany);
+				//Socket socket = new Socket(ipWyizolowany, portWyizolowany);
+				Socket socket = new Socket(ipWyizolowany, 443);
 				
 				connected = true;
 				while (connected) {
@@ -117,7 +259,7 @@ public class GanduClient extends Activity {
                     	//GG_STATUS_AVAIL_DESCR	0x0004	Dostepny (z opisem)
 						int numergg = Integer.parseInt(ggNumberEdit.getText().toString());
 						String haslogg = ggPasswordEdit.getText().toString();
-                    	Logowanie logowanie = new Logowanie(ziarno, haslogg, numergg, 0x0004, (byte)0xff, "Gandu szal:]");
+                    	Logowanie logowanie = new Logowanie(ziarno, haslogg, numergg, 0x0004, (byte)0xff, "Gandu sza³:]");
                     	byte[] paczkalogowania = logowanie.pobraniePaczkiBajtow();
                     	DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                     	//wyslanie paczki logowania 
@@ -163,6 +305,28 @@ public class GanduClient extends Activity {
                     	{
                     		Log.i("Blad autoryzacji", "Typ bledu: "+typOdpSerw);
                 		}
+                    	
+                    	//START odpalenie serwisu
+                    	/*
+                    	startService(new  Intent(GanduClient.this,serwisGandu.class));
+                    	ServiceConnection polaczenie = new ServiceConnection() {
+							
+							@Override
+							public void onServiceDisconnected(ComponentName name) {
+								// TODO Auto-generated method stub
+								
+							}
+							
+							@Override
+							public void onServiceConnected(ComponentName name, IBinder service) {
+								// TODO Auto-generated method stub
+								
+							}
+						};*/
+                    	/*Intent serviceIntent = new Intent();
+                    	serviceIntent.setAction("android.pp.serwisGandu");
+                    	startService(serviceIntent);*/
+                    	//KONIEC odpalenie serwisu
 					} catch (Exception e) {
 						Log.e("ClientActivity", "S: Error", e);
 						connected = false;
