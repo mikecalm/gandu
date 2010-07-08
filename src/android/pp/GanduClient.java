f2d108ecc879b7edb07a2894813468a03e3aeb5a
urbaniakm.we <urbaniakm.we@localhost>
2010-07-08 20:14:12 +0000

    Zmiana systemu interpretacji komunikat?w serwera gg

--- src/android/pp/GanduClient.java
+++ src/android/pp/GanduClient.java
@@ -84,11 +84,6 @@ public class GanduClient extends Activity {
 			{
 				Log.e("Blad","Blad!!!!\n"+excccc.getMessage());
 			}
-			//if (!connected && serverIpAddress != "") {
-				//Thread cThread = new Thread(new ClientThread());
-				//cThread.start();
-            	//startService(new  Intent(GanduClient.this,serwisGandu.class));
-			//}
 		}
 	};
 	
@@ -100,9 +95,6 @@ public class GanduClient extends Activity {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
-                case GanduService.MSG_SET_VALUE:
-                    //mCallbackText.setText("Received from service: " + msg.arg1);
-                    break;
                 case GanduService.MSG_LOGIN:
                 	break;
                 default:
@@ -136,11 +128,6 @@ public class GanduClient extends Activity {
                 Message msg = Message.obtain(null,
                         GanduService.MSG_REGISTER_CLIENT);
                 msg.replyTo = mMessenger;
-                mService.send(msg);
-
-                // Give it some value as an example.
-                msg = Message.obtain(null,
-                        GanduService.MSG_SET_VALUE, this.hashCode(), 0);
                 mService.send(msg);
             } catch (RemoteException e) {
                 // In this case the service has crashed before we could even
@@ -201,142 +188,4 @@ public class GanduClient extends Activity {
             //mCallbackText.setText("Unbinding.");
         }
     }
-
-    //!!!!!!!!DO WYWALENIA!!!!!!!!!!!
-	public class ClientThread implements Runnable {
-		public void run() {
-			try {
-				HttpClient httpclient = new DefaultHttpClient();
-				HttpGet httpget = new HttpGet(
-						"http://appmsg.gadu-gadu.pl/appsvc/appmsg_ver8.asp?fmnumber="+ggNumberEdit.getText().toString()+"&lastmsg=20429&version=10.0.0.10450");
-				HttpResponse response = httpclient.execute(httpget);
-				HttpEntity entity = response.getEntity();
-
-				if (entity != null) {
-					InputStream instream = entity.getContent();
-					byte[] tmp = new byte[2048];
-					while (instream.read(tmp) != -1) {
-						ByteArrayInputStream bais = new ByteArrayInputStream(tmp);
-						BufferedReader br = new BufferedReader(new InputStreamReader(bais));
-						String readline = br.readLine();
-						Log.i("GANDU-TESTING", readline);
-						ip = readline.split(" ");
-					}
-				}
-				Log.d("ClientActivity", "Polaczenie....");
-
-				String ipWyizolowany = ip[2].split(":")[0];
-				int portWyizolowany = Integer.parseInt(ip[2].split(":")[1]);
-				//Socket socket = new Socket(ipWyizolowany, portWyizolowany);
-				Socket socket = new Socket(ipWyizolowany, 443);
-				
-				connected = true;
-				while (connected) {
-					try {
-						// BufferedReader in = new BufferedReader(new
-						// InputStreamReader(sslsocket.getInputStream()));
-						// BufferedReader in = new BufferedReader(new
-						// InputStreamReader(socket.getInputStream()));
-						DataInputStream in = new DataInputStream(socket
-								.getInputStream());
-						int typKom = Integer.reverseBytes(in.readInt());
-						int dlugoscDanych = Integer.reverseBytes(in.readInt());
-						int ziarno = Integer.reverseBytes(in.readInt());
-						Log.i("GANDU-TESTING typ komunikatu: ", "" + typKom);
-						Log.i("GANDU-TESTING dlugosc danych: ", ""
-								+ dlugoscDanych);
-						Log.i("GANDU-TESTING ziarno: ", "" + ziarno);
-						// po podsluchaniu wiresharkiem i wychwyceniu paczki z
-						// ziarnem
-						// i przeliczeniu go z systemu szesnastkowego na
-						// dziesietny
-						// wartosc ziarna zgadza sie z ta uzyskana w zmiennej
-						// int ziarno
-						// wiec jest dobrze, teraz mozna dalej kombinowac ze
-						// skompletowaniem
-						// paczki ktora pozniej trzeba odeslac;)
-
-                    	//GG_STATUS_AVAIL_DESCR	0x0004	Dostepny (z opisem)
-						int numergg = Integer.parseInt(ggNumberEdit.getText().toString());
-						String haslogg = ggPasswordEdit.getText().toString();
-                    	Logowanie logowanie = new Logowanie(ziarno, haslogg, numergg, 0x0004, (byte)0xff, "Gandu sza³:]");
-                    	byte[] paczkalogowania = logowanie.pobraniePaczkiBajtow();
-                    	DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                    	//wyslanie paczki logowania 
-                    	//#define GG_LOGIN80 0x0031
-                    	//do serwera
-                    	out.write(paczkalogowania);
-                    	out.flush();
-                    	//po prawidlowym zalogowaniu sie serwer zawraca pakiet typu
-                    	//#define GG_LOGIN80_OK 0x0035
-                    	//w przypadku bledy autoryzacji otrzymamy pakiet
-                    	//#define GG_LOGIN80_FAILED 0x0043
-                    	int typOdpSerw = Integer.reverseBytes(in.readInt());
-                    	int dlugoscOdpSerw = Integer.reverseBytes(in.readInt());
-                    	int poleUnknownOdpSerw = 0;
-                    	if(dlugoscOdpSerw != 0)
-                    		poleUnknownOdpSerw = Integer.reverseBytes(in.readInt());
-                    	if(typOdpSerw == 0x00000035)
-                		{
-                    		Log.i("Zalogowany", "Pole unknown: "+poleUnknownOdpSerw);
-                    		ggNumberEdit.post(new Runnable() 
-                    		{
-								@Override
-								public void run() {
-									ggNumberEdit.setText("Zalogowany");
-								}
-                    		});
-                    		//wyslanie do serwera GG pakietu (o zerowej d³ugoœci) 
-                    		//z informacja ze nie mamy nikogo na liscie kontaktow
-                    		//#define GG_LIST_EMPTY 0x0012
-                    		ByteArrayOutputStream baos2 = new ByteArrayOutputStream();
-                			DataOutputStream dos2 = new DataOutputStream(baos2);
-                			dos2.writeInt(Integer.reverseBytes(0x0012));
-                			dos2.writeInt(Integer.reverseBytes(0x0000));
-                    		out.write(baos2.toByteArray());
-                    		//odebranie odpowiedzi serwera
-                    		int typOdpSerw2 = Integer.reverseBytes(in.readInt());
-                        	int dlugoscOdpSerw2 = Integer.reverseBytes(in.readInt());
-                        	byte[] resztaOdp = new byte[dlugoscOdpSerw2];
-                        	if(dlugoscOdpSerw2 != 0)
-                        		in.read(resztaOdp, 0, dlugoscOdpSerw2);
-                		}
-                    	else
-                    	{
-                    		Log.i("Blad autoryzacji", "Typ bledu: "+typOdpSerw);
-                		}
-                    	
-                    	//START odpalenie serwisu
-                    	/*
-                    	startService(new  Intent(GanduClient.this,serwisGandu.class));
-                    	ServiceConnection polaczenie = new ServiceConnection() {
-							
-							@Override
-							public void onServiceDisconnected(ComponentName name) {
-								// TODO Auto-generated method stub
-								
-							}
-							
-							@Override
-							public void onServiceConnected(ComponentName name, IBinder service) {
-								// TODO Auto-generated method stub
-								
-							}
-						};*/
-                    	/*Intent serviceIntent = new Intent();
-                    	serviceIntent.setAction("android.pp.serwisGandu");
-                    	startService(serviceIntent);*/
-                    	//KONIEC odpalenie serwisu
-					} catch (Exception e) {
-						Log.e("ClientActivity", "S: Error", e);
-						connected = false;
-					}
-				}
-				socket.close();
-				Log.d("ClientActivity", "C: Closed.");
-			} catch (Exception e) {
-				;
-			}
-		}
-	}
 }
