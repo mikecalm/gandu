fb7d963f4ea2afa54b3d649c10c8f052303b1537
urbaniakm.we <urbaniakm.we@localhost>
2010-07-13 19:51:49 +0000

    small changes in UI

--- src/android/pp/ContactBook.java
+++ src/android/pp/ContactBook.java
@@ -11,7 +11,6 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
-import android.pp.GanduClient.IncomingHandler;
 import android.util.Log;
 import android.widget.TextView;
 
