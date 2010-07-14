0b997c039693396c7c5927160edc1342d1948219
mihoo <mihoo@localhost>
2010-07-14 17:40:30 +0200

    testing XML parsing

---
+++ src/android/pp/XMLHandler.java
@@ -0,0 +1,101 @@
+package android.pp;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class XMLHandler extends DefaultHandler{
+	
+	/**
+	 * Names of marks in .XML file with contactbook
+	 */
+	private boolean inContactBook = false;
+	private boolean inGroups = false;
+	private boolean inGroup = false;
+	private boolean inId = false;
+	private boolean inName = false;
+	private boolean inIsExpanded = false;
+	private boolean inIsRemovable = false;
+	private boolean inContacts = false;
+	
+	private XMLParsedDataSet pds = new XMLParsedDataSet();
+	
+	public  XMLParsedDataSet getParsedData() {
+		return this.pds;
+	}
+	
+	@Override
+	public void startDocument() throws SAXException
+	{
+		this.pds = new XMLParsedDataSet();
+	}
+	
+	@Override
+	public void endDocument() throws SAXException
+	{
+		//  ntd
+	}
+	
+	@Override
+	public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException
+	{
+		if (localName.equals("ContactBook"))
+		{
+			this.inContactBook = true;
+		}
+		else if (localName.equals("Groups"))
+		{
+			this.inGroups = true;
+		}
+		else if(localName.equals("Group"))
+		{
+			this.inGroup = true;
+		}
+		else if (localName.equals("Id"))
+		{
+			this.inId = true;
+		}
+		else if (localName.equals("Name"))
+		{
+			this.inName = true;
+			
+		}
+	}
+	
+	@Override
+	public void endElement(String namespaceURI, String localName, String qName) throws SAXException
+	{
+		if (localName.equals("ContactBook"))
+		{
+			this.inContactBook = false;
+		}
+		else if (localName.equals("Groups"))
+		{
+			this.inGroups = false;
+		}
+		else if(localName.equals("Group"))
+		{
+			this.inGroup = false;
+		}
+		else if (localName.equals("Id"))
+		{
+			this.inId = false;
+		}
+		else if (localName.equals("Name"))
+		{
+			this.inName = false;
+			
+		}
+	}
+	
+	@Override
+	public void characters(char ch[], int start, int length)
+	{
+		if(this.inName)
+		{
+			pds.setExtractedString(new String(ch, start, length));
+			
+		}
+	}
+	
+}
