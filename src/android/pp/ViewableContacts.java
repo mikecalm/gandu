32e834e0e75dd78cffa3b43cf5f9252a281c474f
spaj <spaj@localhost>
2011-01-31 18:18:24 +0100

    + sorting contacts by status

--- src/android/pp/ViewableContacts.java
+++ src/android/pp/ViewableContacts.java
@@ -17,7 +17,19 @@ public class ViewableContacts implements Comparable{
 	@Override
 	public int compareTo(Object another) {
 		String thisVal = this.showName;
+		//jesli kontakt ma status niedostepny, niedostepny z opisem lub ignorowany
+		if(this.status == 1 || this.status == 15 || this.status == 6)
+			thisVal = "2"+thisVal;
+		//jesli kontakt jest dostepny, zaraz wracam, poggadaj itp.
+		else
+			thisVal = "1"+thisVal;
 		String anotherVal = ((ViewableContacts)another).showName;
+		//jesli kontakt ma status niedostepny, niedostepny z opisem lub ignorowany
+		if(((ViewableContacts)another).status == 1 || ((ViewableContacts)another).status == 15 || ((ViewableContacts)another).status == 6)
+			anotherVal = "2"+anotherVal;
+		//jesli kontakt jest dostepny, zaraz wracam, poggadaj itp.
+		else
+			anotherVal = "1"+anotherVal;
 		return thisVal.compareToIgnoreCase(anotherVal);
 	}
 
