cb9790105b0acb5f33d37dee0e3b1f0477b2395a
spaj <spaj@localhost>
2011-01-31 19:54:03 +0100

    + r?wne traktowanie dla polskich ogonk?w na li?cie kontakt?w!

--- src/android/pp/ViewableContacts.java
+++ src/android/pp/ViewableContacts.java
@@ -1,5 +1,7 @@
 package android.pp;
 
+import java.util.Hashtable;
+
 public class ViewableContacts implements Comparable{
 	
 	public String showName;
@@ -17,6 +19,7 @@ public class ViewableContacts implements Comparable{
 	@Override
 	public int compareTo(Object another) {
 		String thisVal = this.showName;
+		thisVal = Common.wyciecieOgonkow(thisVal);
 		//jesli kontakt ma status niedostepny, niedostepny z opisem lub ignorowany
 		if(this.status == Common.GG_STATUS_NOT_AVAIL || this.status == Common.GG_STATUS_NOT_AVAIL_DESCR || this.status == Common.GG_STATUS_BLOCKED)
 			thisVal = "2"+thisVal;
@@ -24,6 +27,7 @@ public class ViewableContacts implements Comparable{
 		else
 			thisVal = "1"+thisVal;
 		String anotherVal = ((ViewableContacts)another).showName;
+		anotherVal = Common.wyciecieOgonkow(anotherVal);
 		//jesli kontakt ma status niedostepny, niedostepny z opisem lub ignorowany
 		if(((ViewableContacts)another).status == Common.GG_STATUS_NOT_AVAIL || ((ViewableContacts)another).status == Common.GG_STATUS_NOT_AVAIL_DESCR || ((ViewableContacts)another).status == Common.GG_STATUS_BLOCKED)
 			anotherVal = "2"+anotherVal;
@@ -31,6 +35,6 @@ public class ViewableContacts implements Comparable{
 		else
 			anotherVal = "1"+anotherVal;
 		return thisVal.compareToIgnoreCase(anotherVal);
-	}
+	}	
 
 }
